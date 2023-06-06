b0e8d01025e49c2e2f66f56ded3a8ce98f59a767
tarek-kidwai <tarek.kid@gmail.com>
2023-06-07 03:42:32 +1000

    Changes added

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-let form = document.getElementById("form");
-let textInput = document.getElementById("textInput");
-let dateInput = document.getElementById("dateInput");
-let textarea = document.getElementById("textarea");
-let msg = document.getElementById("msg");
-let tasks = document.getElementById("tasks");
-let add = document.getElementById("add");
+const idSelector = (id) => document.getElementById(id);
+
+let form = idSelector("form"),
+  textInput = idSelector("textInput"),
+  dateInput = idSelector("dateInput"),
+  textarea = idSelector("textarea"),
+  msg = idSelector("msg"),
+  tasks = idSelector("tasks"),
+  add = idSelector("add"),
+  selectedPerson = idSelector("select-person"),
+  selectedStatus = idSelector("selected-status");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -35,17 +39,33 @@ let acceptData = () => {
   data["text"] = textInput.value;
   data["date"] = dateInput.value;
   data["description"] = textarea.value;
+  data["assignTo"] = selectedPerson.value;
+  data["status"] = selectedStatus.value;
 
   createTasks();
-
 };
 
 let createTasks = () => {
   tasks.innerHTML += `<div>
       <span class="fw-bold">${data.text}</span>
+      <span class="small text-secondary">${data.assignTo}</span>
       <span class="small text-secondary">${data.date}</span>
       <p>${data.description}</p>
-
+      ${
+        data?.status === "Completed"
+          ? '<span class="badge bg-success">' + data?.status + "</span>"
+          : ""
+      }
+      ${
+        data?.status === "In progress"
+          ? '<span class="badge bg-primary">' + data?.status + "</span>"
+          : ""
+      }
+      ${
+        data?.status === "In Review"
+          ? '<span class="badge bg-warning">' + data?.status + "</span>"
+          : ""
+      }
       <span class="options">
         <i onClick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
         <i onClick="deleteTask(this) "class="fas fa-trash-alt"></i>
